b600c1de15c33c26a8b7a7528a7ff45678d840ab
Tugay <Tugay@DESKTOP-8EHVPE9>
2023-08-24 03:06:50 +0300

    IdentityRole

---
+++ CoreForIdentity/Model/Concrete/TypeConfiguration/Concrete/IdentityRoleMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreForIdentity.Model.Concrete.TypeConfiguration.Concrete
+{
+    public class IdentityRoleMap : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            // Rolleri buradan ekliyoruz
+            builder.HasData(new IdentityRole { Id = Guid.NewGuid().ToString(), Name = "Admin", NormalizedName = "ADMIN" });
+        }
+    }
+}
